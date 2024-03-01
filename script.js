b5e3ce13e6d3633a80a64967fa314bdc3bebf2df
AgalyaSuresh <146180429+AgalyaSuresh@users.noreply.github.com>
2024-03-01 09:59:13 +0530

    HTML

---
+++ script.js
@@ -0,0 +1,13 @@
+// You can add JavaScript code here
+// For example, you can add functionality like smooth scrolling, form validation, etc.
+
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
